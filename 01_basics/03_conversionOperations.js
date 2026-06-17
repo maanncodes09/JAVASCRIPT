80912eb405373e156cbefc6689fb47c78464f0dc
Mansi Agarwal <mansiagarwal.1805@gmail.com>
2026-06-17 14:34:23 +0000

    Practice on conversions

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let score="abc90"
+console.log(typeof(score)); // declaring score under parantheses is a method way
+//can also declare it in without parantheses
+let valueInNumber=Number(score)
+console.log(typeof valueInSNumber);
+console.log(valueInNumber); // output NaN, not a number, when cannot convert
+let val=null
+console.log(typeof val);
+let valInNumber=Number(val)
+console.log(typeof valInSNumber);
+console.log(valInNumber); // output will be 0
+
+//for undefined value will be NaN
+
+let type=true
+console.log(typeof type);
+console.log(type);
+let typInNumber=Number(type)
+console.log(typeof typInNumber);
+console.log(typInNumber); // 1
+let typInString=String(type)
+console.log(typeof typInString);
+console.log(typInString); // true
+
