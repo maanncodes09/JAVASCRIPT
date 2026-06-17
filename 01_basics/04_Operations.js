2ea864c50c09c44aba353c5bfb52c6094eb039de
Mansi Agarwal <mansiagarwal.1805@gmail.com>
2026-06-17 18:28:48 +0000

    Practice on Operations

---
+++ 01_basics/04_Operations.js
@@ -0,0 +1,13 @@
+let val=10
+let negval=-val
+console.log(negval);
+let str1="Hello,"
+let str2=" World!"
+let str3=str1+str2
+console.log(str3);
+console.log("1"+2); //12
+console.log(1+"2"); //12
+console.log("1"+2+2); //122
+console.log(1+2+"2"); //32 
+console.log(+true);
+console.log(+"");
