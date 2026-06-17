53361c756b9a5fe69f68eb0d6e45968a51e39163
Mansi Agarwal <mansiagarwal.1805@gmail.com>
2026-06-17 13:53:57 +0000

    Practice on JS basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=111899
+let accountEmail="sky09@gmail.com"
+var accountPassword="121212"
+accountCity="Hyderabad"
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+let accountState;
+accountEmail="sky1919@gmail.com"
+accountPassword="19191919"
+accountCity="Pune"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+// js engine is hidden in browser
+/*"use strict";*/ // this gonna treat all JS code as newer version and yeah we cannot reverse
+let name="sky"
+let age=20
+let accountloggedIn=true
+let accountType;
+
+console.log(typeof(age));
+console.log(typeof(name));
+console.log(typeof(accountloggedIn));
+console.log(typeof(undefined)); //undefined
+console.log(typeof(accountType)); //undefined
+console.log(typeof null); //object
