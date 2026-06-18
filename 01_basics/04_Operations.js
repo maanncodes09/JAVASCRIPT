a8388c91d580da866c06822eaed322407b8125ec
Mansi Agarwal <mansiagarwal.1805@gmail.com>
2026-06-18 17:44:10 +0000

    more practice on operations and comparisons

--- 01_basics/04_Operations.js
+++ 01_basics/04_Operations.js
@@ -11,3 +11,18 @@ console.log("1"+2+2); //122
 console.log(1+2+"2"); //32 
 console.log(+true);
 console.log(+"");
+//************comparison******************
+
+console.log(null>0); //false
+console.log(null==0); //false this didn't
+console.log(null>=0); //true
+// >,<,>=,<= works differently they change null into 0 then compare
+console.log("02">1); //true
+console.log(undefined==0); //false
+console.log(undefined>=0); //false
+console.log(undefined<=0); //false
+
+/* In JS equality check and comparison works differently
+'===' it is a strict check, it not only check value , it also checks data types*/
+console.log("2"==2); // true, because it only compares
+console.log("2"===2); //false, it checks datatype also
