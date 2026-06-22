b3100b29e223901aef810e185fb3f1aaeeff8c6e
Mansi Agarwal <mansiagarwal.1805@gmail.com>
2026-06-22 03:18:10 +0000

    Practice on Primitive & Non-Primitive

---
+++ 01_basics/05_datatypessummary.js
@@ -0,0 +1,9 @@
+const heros=["spideran", "ironman", "hulk"] // arrays (non-primitive)
+let myobj={
+    name:Sky,
+    age:20,
+} // object
+const myFunction=function(){
+    console.log("Hello, World!");
+    
+} //function
